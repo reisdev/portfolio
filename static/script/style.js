851ece1744f4c618845acace55326cebdeb83ae3
Matheus dos Reis de Jesus <matheusdrdj@gmail.com>
2019-08-31 15:53:33 -0300

    Fixing menu styles

--- static/script/style.js
+++ static/script/style.js
@@ -1,16 +1,20 @@
-function toggleMenu(event){
-    const offset = this.scrollY;
-    if (offset < 100) {
-      $(".menu").addClass("big");
-      $(".scroll-to-top").removeClass("visible")
-    } else {
-      $(".menu").removeClass("big");
-      $(".scroll-to-top").addClass("visible")
-    }
+function toggleMenu(event) {
+  const offset = this.scrollY;
+  if (offset < 150) {
+    $(".menu")
+      .removeClass("floating")
+      .addClass("big");
+    $(".scroll-to-top").removeClass("visible");
+  } else {
+    $(".menu")
+      .addClass("floating")
+      .removeClass("big");
+    $(".scroll-to-top").addClass("visible");
+  }
 }
 
 function load() {
-  window.addEventListener("scroll",toggleMenu);
+  window.addEventListener("scroll", toggleMenu);
   toggleMenu();
 }
 
