726a3084c5caf3ab0598edfb21431eef89ca5580
Matheus dos Reis de Jesus <matheusdrdj@gmail.com>
2019-11-01 19:30:37 -0300

    [feat] (layout): Gulp setup + Layout refactor

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,7 +4,14 @@ const useref = require("gulp-useref");
 const uglify = require("gulp-uglify");
 const cssnano = require("gulp-cssnano");
 const rename = require("gulp-rename");
-const del = require("del");
+const image = require("gulp-image");
+
+gulp.task("image", function() {
+  return gulp
+    .src("src/static/img/*.{png,jpg,svg}")
+    .pipe(image())
+    .pipe(gulp.dest("dist/img"));
+});
 
 gulp.task("js", function() {
   return (
@@ -27,13 +34,12 @@ gulp.task("css", function() {
     .src("src/static/css/*style.css")
     .pipe(cssnano())
     .pipe(rename({ suffix: ".min" }))
-    .pipe(del(["style.min.css"]))
     .pipe(gulp.dest("dist/css")); // Outputs the file in the destination folder
 });
 
 gulp.task(
   "build",
-  gulp.series(["js", "css"], function() {
+  gulp.series(["image", "js", "css"], function() {
     return gulp
       .src("src/index.html")
       .pipe(useref())
