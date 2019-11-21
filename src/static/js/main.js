e8793bc64c3e2c639274f67431f73592474090bc
Matheus dos Reis de Jesus <matheusdrdj@gmail.com>
2019-11-21 19:35:57 -0300

    [refactor] (style): Changing sessions order and new menu style

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,6 +1,7 @@
 function load() {
-  $("section .header").click(el => {
-    copyToClipboard($(el.target).data("target"));
+  document.addEventListener("scroll", e => {
+    if (window.scrollY >= 363) $(".menu")[0].classList.add("colored");
+    else $(".menu")[0].classList.remove("colored");
   });
 }
 
@@ -17,7 +18,9 @@ function toggleMenu() {
   const menu = $(".menu > section:not(.content) > .topics")[0];
   if (menu.classList.contains("active")) {
     menu.classList.remove("active");
+    $(".menu")[0].classList.remove("colored");
   } else {
     menu.classList.add("active");
+    $(".menu")[0].classList.add("colored");
   }
 }
