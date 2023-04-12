46fec3e26119456a6664a5de324942f8872cc0ee
ReisDev <matheusdrdj@gmail.com>
2023-04-12 10:06:20 -0300

    [chore]: removing unused packages and tests

--- src/hooks/useAnalyticsEventTracker.tsx
+++ src/hooks/useAnalyticsEventTracker.tsx
@@ -1,4 +1,4 @@
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 
 const useAnalyticsEventTracker = (category: string) => {
     const tracker = (action = "test action", label = "") => {
